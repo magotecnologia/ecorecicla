ec53cfe7e9cffb3a54473231666944e6c2deacd3
Marco Gonzalez <marco@magotecnologia.com>
2023-07-24 22:27:17 -0500

    *Se coloca elementos esenciales en el layout de fragment, no se realiza diseño *Se baja version de lifecycle por problemas de compatibilidad *Se coloca dependencia de platforms para evitar duplicados de clases *Se añade viewbiding como feature

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,9 +1,11 @@
 object Versions {
+    const val KOTLIN="1.8.0"
+
     const val GRADLE="8.0.2"
     const val APP_COMPAT="1.6.1"
     const val MATERIAL="1.5.0"
     const val CONSTRAINT="2.1.4"
     const val JUNIT="4.13.2"
     const val NAVIGATION="2.6.0"
-    const val LIFECYCLE="2.6.1"
+    const val LIFECYCLE="2.5.1"
 }
