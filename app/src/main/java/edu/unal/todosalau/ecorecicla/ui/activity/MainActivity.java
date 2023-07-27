45250afd68229d685e7def3f487c8b03bc08f3c5
Daniel <drespinosa95@gmail.com>
2023-07-26 23:30:25 -0500

    Se añade menu inferior bottom_nav_menu.xml para manejar la navegación de vistas asociadas a la pantalla home en la actividad principal.

--- app/src/main/java/edu/unal/todosalau/ecorecicla/ui/activity/MainActivity.java
+++ app/src/main/java/edu/unal/todosalau/ecorecicla/ui/activity/MainActivity.java
@@ -1,16 +1,35 @@
 package edu.unal.todosalau.ecorecicla.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import edu.unal.todosalau.ecorecicla.R;
+import edu.unal.todosalau.ecorecicla.databinding.ActivityLoginBinding;
+import edu.unal.todosalau.ecorecicla.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        navigateToCategoryFragment();
+    }
+
+    public void navigateToCategoryFragment() {
+        navController.navigate(R.id.action_homeFragment_to_categoryFragment);
     }
 }
