36f800526e24c23732399979fb4bc9916d624c2d
Marco Gonzalez <marco@magotecnologia.com>
2023-07-27 23:52:57 -0500

    Se arregla navegacion usando BottomNavigationView

--- app/src/main/java/edu/unal/todosalau/ecorecicla/ui/activity/MainActivity.java
+++ app/src/main/java/edu/unal/todosalau/ecorecicla/ui/activity/MainActivity.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import edu.unal.todosalau.ecorecicla.R;
 import edu.unal.todosalau.ecorecicla.databinding.ActivityMainBinding;
@@ -19,8 +22,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        navigateToCategoryFragment();
+        BottomNavigationView navView = binding.navView;
+        final NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_activity_main);
+        final NavController navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(navView, navController);
     }
 
     public void navigateToCategoryFragment() {
--- app/src/main/java/edu/unal/todosalau/ecorecicla/ui/fragment/StatsFragment.java
+++ app/src/main/java/edu/unal/todosalau/ecorecicla/ui/fragment/StatsFragment.java
@@ -39,12 +39,12 @@ public class StatsFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         viewModel = new ViewModelProvider(this).get(StatsViewModel.class);
-        PieChart.Slice paperSlice = buildBadicSlice(0.2F, "PAPEL");
-        PieChart.Slice glassSlice = buildBadicSlice(0.4F, "VIDRIO");
+        PieChart.Slice paperSlice = buildBasicSlice(0.2F, "PAPEL");
+        PieChart.Slice glassSlice = buildBasicSlice(0.4F, "VIDRIO");
         binding.pieChart.setSlices(new ArrayList<>(Arrays.asList(paperSlice, glassSlice)));
     }
 
-    PieChart.Slice buildBadicSlice(Float fraction, String legend) {
+    PieChart.Slice buildBasicSlice(Float fraction, String legend) {
         return new PieChart.Slice(fraction, Color.rgb(120, 181, 0), Color.rgb(149, 224, 0), NumberFormat.getPercentInstance().format(fraction), null, null, null, null, null, null, null, null, null, null, null, legend, null, null, null, null, null, null, null, null, null, 1f, 1f);
     }
 
