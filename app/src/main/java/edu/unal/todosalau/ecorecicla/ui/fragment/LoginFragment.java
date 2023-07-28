9c4f486cd17622f7f84160bbe57e25053d7cfd11
Marco Gonzalez <marco@magotecnologia.com>
2023-07-27 22:23:32 -0500

    *Se ordena separando fragments de viewmodels *Se realiza cambios en el diseño visual del login

--- app/src/main/java/edu/unal/todosalau/ecorecicla/ui/fragment/LoginFragment.java
+++ app/src/main/java/edu/unal/todosalau/ecorecicla/ui/fragment/LoginFragment.java
@@ -14,9 +14,11 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 
 import edu.unal.todosalau.ecorecicla.R;
+import edu.unal.todosalau.ecorecicla.databinding.FragmentLoginBinding;
+import edu.unal.todosalau.ecorecicla.ui.viewmodel.LoginViewModel;
 
 public class LoginFragment extends Fragment {
-//    private FragmentLoginBinding binding;
+    private FragmentLoginBinding binding;
     private LoginViewModel viewModel;
 
     private NavController navController;
@@ -28,20 +30,16 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-//        binding = FragmentLoginBinding.inflate(inflater, container, false);
-//        return binding.getRoot();
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        binding = FragmentLoginBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         navController = findNavController(this);
-        viewModel = new ViewModelProvider(this).get(LoginViewModel.class);
-        viewModel.state.observe(getViewLifecycleOwner(), loginState -> {
-            if (loginState.getLogged()) {
-                //Manda a la otra actividad
-            }
+        viewModel = new ViewModelProvider(requireActivity()).get(LoginViewModel.class);
+        viewModel.getState().observe(getViewLifecycleOwner(), loginState -> {
             if (!loginState.getError().isEmpty()) {
                 //Muestra el error
             }
@@ -50,15 +48,15 @@ public class LoginFragment extends Fragment {
     }
 
     private void setClickEvents() {
-//        binding.loginButton.setOnClickListener(view -> {
-//            //viewModel.login(binding.txtUser.getText().toString(), binding.txtUser.getText().toString());
-//        });
-//        binding.btnRegister.setOnClickListener(view -> {
-//            navController.navigate(R.id.action_from_login_to_register);
-//        });
-//        binding.btnPasswordRecovery.setOnClickListener(view -> {
-//            navController.navigate(R.id.action_from_login_to_restore);
-//        });
+        binding.btnLogin.setOnClickListener(view -> {
+            viewModel.login(binding.etUser.getText().toString(), binding.etPassword.getText().toString());
+        });
+        binding.tvRegister.setOnClickListener(view -> {
+            navController.navigate(R.id.action_from_login_to_register);
+        });
+        binding.tvPasswordRecovery.setOnClickListener(view -> {
+            navController.navigate(R.id.action_from_login_to_restore);
+        });
 
     }
 
@@ -66,6 +64,6 @@ public class LoginFragment extends Fragment {
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-//        binding = null;
+        binding = null;
     }
 }
