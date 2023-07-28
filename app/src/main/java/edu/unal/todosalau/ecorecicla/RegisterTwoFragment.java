f5c22011c3081d032fb21c76b033da9572a8f1e4
Marco Gonzalez <marco@magotecnologia.com>
2023-07-27 20:42:56 -0500

    Se crea un segundo fragmento para el registro

---
+++ app/src/main/java/edu/unal/todosalau/ecorecicla/RegisterTwoFragment.java
@@ -0,0 +1,34 @@
+package edu.unal.todosalau.ecorecicla;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+public class RegisterTwoFragment extends Fragment {
+
+    private RegisterTwoViewModel mViewModel;
+
+    public static RegisterTwoFragment newInstance() {
+        return new RegisterTwoFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_register_two, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(RegisterTwoViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
