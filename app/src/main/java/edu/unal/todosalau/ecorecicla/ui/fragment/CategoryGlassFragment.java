641811371461d8522b36ec69eab5342f974b5c6e
Marco Gonzalez <marco@magotecnologia.com>
2023-07-28 22:06:11 -0500

    Se incluyo binding para dos fragmentos

--- app/src/main/java/edu/unal/todosalau/ecorecicla/ui/fragment/CategoryGlassFragment.java
+++ app/src/main/java/edu/unal/todosalau/ecorecicla/ui/fragment/CategoryGlassFragment.java
@@ -10,10 +10,11 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import edu.unal.todosalau.ecorecicla.R;
+import edu.unal.todosalau.ecorecicla.databinding.FragmentCategoryPaperBinding;
 import edu.unal.todosalau.ecorecicla.ui.viewmodel.RecordRecycleViewModel;
 
 public class CategoryGlassFragment extends Fragment {
+    private FragmentCategoryPaperBinding binding;
 
     private RecordRecycleViewModel mViewModel;
 
@@ -24,14 +25,14 @@ public class CategoryGlassFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_category_glass, container, false);
+        binding = FragmentCategoryPaperBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(RecordRecycleViewModel.class);
-        // TODO: Use the ViewModel
     }
 
 }
